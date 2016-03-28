f23199cf52393ab78075a406af66e2189883c165
andela-ekupara <edwin.kupara@andela.com>
2016-03-28 18:19:52 +0300

    Set up appliccation

---
+++ js/app.jsx
@@ -0,0 +1,14 @@
+(function() {
+	'use strict';
+	var Quiz = React.createClass({
+		render: function() {
+			return (
+				<div> 
+					<h1>Author Quiz</h1>
+				</div>
+				)
+		}
+	});
+
+React.render(<Quiz/>, document.getElementById('app'));
+})();
